456a840c505e0b4d402c63fbc0c69a97ec3e66b8
matheusjuda <matheusbj582@gmail.com>
2024-04-17 16:17:44 -0300

    Usando o Winston logger para mostrar erro de API

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import { CardPost } from "@/Componentes/CardPost";
+import logger from "@/logger";
 
 //API json-serve / Função pausada ate await retornar
 async function getAllPosts() {
   const response = await fetch('http://localhost:3042/posts')
   if (!response.ok) {
-    console.log('ERROOOOOO API')
+    logger.error('ERROOOOOO API')
+    return [] //retorna array vazio para não quebrar a aplicação
   }
+  logger.info('Post obtido com sucesso')
   return response.json()
 }
 
