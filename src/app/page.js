2f17453952137eef0e774cdf63b55ce0b7e75840
matheusjuda <matheusbj582@gmail.com>
2024-04-22 15:32:50 -0300

    1 passo Paginando os dados da API

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import logger from "@/logger";
 import styles from './page.module.css'
 
 //API json-serve / Função pausada ate await retornar
-async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts')
+async function getAllPosts(page) {
+  //Pegar dados da Api definindo o numero de 6 posts por pagina./ https://www.npmjs.com/package/json-server#paginate
+  const response = await fetch(`http://localhost:3040/posts?_page=${page}&_per_page=6`) //pegando a pagina por parametro (page)
   if (!response.ok) {
     logger.error('ERROOOOOO API')
     return [] //retorna array vazio para não quebrar a aplicação
@@ -14,10 +15,11 @@ async function getAllPosts() {
 }
 
 export default async function Home() {
-  const post = await getAllPosts()
+  //Na api de page os posts estão dentro do objeto data
+  const { data: post } = await getAllPosts(1)
   return (
     <main className={styles.grid}>
-      {post.map(post => <CardPost post={post} />)}
+      {post.map(post => <CardPost key={post.id} post={post} />)}
 
     </main>
   );
